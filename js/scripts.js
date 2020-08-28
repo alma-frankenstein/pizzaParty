1a9600508d6f3702c36d8c8c42612a4e2adf2d6c
Alma <a.q.frankenstein@gmail.com>
2020-08-28 08:14:24 -0700

    create file tree and outline

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#number").submit(function(event) {
+    event.preventDefault();
+
+    // const userNumber = parseInt($("#inputNum").val());
+
+    // let returnNumber = replacer(ranger(userNumber));
+
+    // $("#returnNumber").text(returnNumber);
+    // $("#returnNumber").show();
+  })
+});
