23197f3543f80ae79ea1d963175046378b52f548
Alma <a.q.frankenstein@gmail.com>
2020-08-28 15:26:31 -0700

    add size and topping properties to constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 
-function Pizza() { 
+function Pizza(size, toppings) { 
   this.totalCost = 0;
+  this.size = size;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.addToppingCost = function(total) {
@@ -17,6 +19,32 @@ function checkForSize(sizeValue){
   return parseInt(size);
 }
 
+function valToSize(numVal){
+  let size;
+  if (numVal === 12){
+    size = "small";
+  } else if (numVal === 14){
+    size = "medium";
+  } else {
+    size = "large";
+  }
+  return size;
+}
+
+function valToTopping(numVal){
+  let topping;
+  if (numVal === 1){
+    topping = "eggplant";
+  } else if (numVal === 2){
+    topping = "Peeps";
+  } else if (numVal === 3){
+    topping = "anchovies";
+  } else {
+    topping = "Lucky Charms";
+  }
+  return topping;
+}
+
 
 $(document).ready(function(){
   $("form#pizzaSelector").submit(function(event) {
@@ -25,15 +53,23 @@ $(document).ready(function(){
     let toppingTotal = 0;
     let sizeChecked = $("input:radio[name=size]:checked").val(); 
     let sizeVal = checkForSize(sizeChecked);
+    let toppingArray = [];
 
     $.each($("input[name='topping']:checked"), function(){
-      toppingTotal += parseInt($(this).val());
+      // toppingTotal += parseInt($(this).val());
+      let toppingVal = parseInt($(this).val());
+      toppingTotal += toppingVal;
+      topping = valToTopping(toppingVal);
+      toppingArray.push(topping);
     });
+    console.log(toppingArray);
 
+    const pizzaSize = valToSize(sizeVal);
     const total = sizeVal + toppingTotal;
 
-    let myPizza = new Pizza();
+    let myPizza = new Pizza(pizzaSize, toppingArray);
     myPizza.addToppingCost(total);
+    console.log(myPizza.size);
 
     $("#cost").text("$" + myPizza.totalCost);
     $("#pizzaCost").show();
